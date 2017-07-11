312e3419b8ab6caa2db018669e594d043c8e5d43
Diana Villasmil <diacavipi@gmail.com>
2017-07-10 23:34:52 -0400

    fin etapa 6 se declara la variable html para almacenar un string en el que se inyectan los valores provenientes de la actividad, se le agrega como contenido a div.list-activities

--- js/script-index.js
+++ js/script-index.js
@@ -81,6 +81,30 @@ function renderActivities(activitiesArray) {
 */
 function renderActivity(activity) {
 
+	var html = `
+		<a href="#" class="item-activity">
+
+		<span class="attribution">
+		
+			<span class="avatar">
+			<img src="${activity.userAvatar}" class="image-avatar">
+			</span>
+			
+			<span class="meta">
+			<span class="author">${activity.userName}</span> made 
+			<span class="recipe">${activity.recipeName}</span>:${activity.text} 
+			<span class="location">&mdash;${activity.place}</span>
+			</span>
+		
+		</span>
+
+		<div class="bg-image" style="background-image: url('${activity.image}');"></div>
+
+		</a>
+	`;
+
+	$('.list-activities').append(html);
+
 }
 
 
