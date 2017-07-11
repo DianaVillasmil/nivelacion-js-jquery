fd9ddc6164f2a43a91dd49ca6917c96e775971b9
Diana Villasmil <diacavipi@gmail.com>
2017-07-10 23:59:52 -0400

    fin etapa 9 se le agrega la clase active a la pestaña que recibe el click, y se le quita a la otra

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -3,9 +3,13 @@ $(document).ready(function() {
 	$(".js-menu").hide();
 	$('a.js-show-recipe').on('click', (event) => {
 		$('.page.recipe').removeClass('make');
+		$('a.js-show-recipe').addClass('active');
+		$('a.js-show-make').removeClass('active');
 	});	
 	$('a.js-show-make').on('click', (event) => {
 		$('.page.recipe').addClass('make');
+		$('a.js-show-make').addClass('active');
+		$('a.js-show-recipe').removeClass('active');
 	});	
 });
 
