5a67ae85b7efd0e82a892362866a32b1408d7fb3
Diana Villasmil <diacavipi@gmail.com>
2017-07-10 23:52:43 -0400

    fin etapa 8 se asignaron funciones que se ejecutan cuando el usuario da click en las pestañas agregando y quitando la clase make

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
 	console.log("El documento html esta listo");
 	$(".js-menu").hide();
-
-	
+	$('a.js-show-recipe').on('click', (event) => {
+		$('.page.recipe').removeClass('make');
+	});	
+	$('a.js-show-make').on('click', (event) => {
+		$('.page.recipe').addClass('make');
+	});	
 });
 
